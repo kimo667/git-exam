dd67d8624affac6898efd192f718d3fbceb5947e
Kml <208549849+kimo667@users.noreply.github.com>
2026-06-05 14:43:14 +0200

    Add color feature

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 )
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "route: %q", r.URL.Path)
 }
 
 func main() {
 	http.HandleFunc("/", IndexHandler)
-	// to add : color functionality
+	http.HandleFunc("/color", ColorHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
